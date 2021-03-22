5089815a30490e465032d0ae4390b1e20b8ed1a8
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-03-22 17:23:08 +0100

    Prettier format - remove semicolons, add bracket spacing, add arrow parens

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Mozilla/Developer_guide/Coding_Style/Formatting_JS_Code_With_Prettier_and_eslint
+// https://eslint.org/docs/user-guide/configuring/rules
+// https://eslint.org/docs/rules/
+
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    'prettier/prettier': 'off',
+    quotes: 'off',
+    'no-trailing-spaces': 'off',
+    semi: 'off',
+  },
+}
--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React from 'react'
 import {
   SafeAreaView,
   ScrollView,
@@ -17,14 +17,14 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native';
+} from 'react-native'
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+  title: string
+}> = ({ children, title }) => {
+  const isDarkMode = useColorScheme() === 'dark'
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -46,15 +46,15 @@ const Section: React.FC<{
         {children}
       </Text>
     </View>
-  );
-};
+  )
+}
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  }
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -73,8 +73,8 @@ const App = () => {
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   sectionContainer: {
@@ -93,6 +93,6 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
-});
+})
 
-export default App;
+export default App
