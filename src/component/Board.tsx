40cc12a8f09a01cfc97f72b737e0706115d95a88
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-03-23 12:38:36 +0100

    Reduce size of cards in landscape so that we can see the whole board without scrolling

--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -15,34 +15,45 @@ for (const cardType in CardType) {
 const cards: Card[] = cardTypes.map((cardType) => new Card(cardType))
 
 export function Board() {
-  const size = useCardSize()
+  const { boardSize, cardSize } = useCardSize()
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.containerPortrait, { width: boardSize }]}>
       {cards.map((card, index) => (
-        <CardView card={card} key={index} cardSize={size} gapSize={GAP_SIZE} />
+        <CardView
+          card={card}
+          key={index}
+          cardSize={cardSize}
+          gapSize={GAP_SIZE}
+        />
       ))}
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    margin: 8,
-  },
-})
-
 // Card size - https://stackoverflow.com/q/29394297/4034572
 
 const COLUMN_COUNT = 4
-export const GAP_SIZE = 8
+const GAP_SIZE = 8
+const VERTICAL_SPACE_ON_LANDSCAPE = 80
 
-function useCardSize(): number {
-  const windowWidth = useWindowDimensions().width
-  // We use COLUMN_COUNT + 1 because there is gap at the left and right of each
-  // column, and at the lef and right of the board
-  const size = (windowWidth - GAP_SIZE * 2 * (COLUMN_COUNT + 1)) / COLUMN_COUNT
-  return size
+function useCardSize(): { boardSize: number; cardSize: number } {
+  const { width, height } = useWindowDimensions()
+  const isPortrait = height > width
+  const size = isPortrait ? width : height - VERTICAL_SPACE_ON_LANDSCAPE
+  // We use COLUMN_COUNT + 1 because there is gap at the left and right of
+  // each column, and at the lef and right of the whole board
+  return {
+    boardSize: size,
+    cardSize: (size - GAP_SIZE * 2 * (COLUMN_COUNT + 1)) / COLUMN_COUNT,
+  }
 }
+
+const styles = StyleSheet.create({
+  containerPortrait: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignSelf: 'center',
+    padding: GAP_SIZE,
+  },
+})
