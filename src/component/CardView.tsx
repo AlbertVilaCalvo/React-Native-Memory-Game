103e9e16c7414cdeba0845ee8e0e83bf03b3cb60
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-03-29 13:28:23 +0200

    CardView - extract animations to custom hooks

--- src/component/CardView.tsx
+++ src/component/CardView.tsx
@@ -14,6 +14,75 @@ const MATCH_STEP_DURATION = 120
 const NO_MATCH_STEP_DURATION = 120
 export const NO_MATCH_ANIMATION_DURATION = NO_MATCH_STEP_DURATION * 5
 
+function useMatchAnimation() {
+  const runMatchAnimation = useSharedValue(false)
+  const matchAnimationStyle = useAnimatedStyle(() => {
+    // Code here runs on the UI thread, so it will be synchronous
+    if (!runMatchAnimation.value) {
+      return {
+        transform: [{ scaleX: 1 }, { scaleY: 1 }],
+      }
+    }
+    return {
+      transform: [
+        {
+          scaleX: withSequence(
+            withTiming(1.4, { duration: MATCH_STEP_DURATION }),
+            withTiming(0.7, { duration: MATCH_STEP_DURATION * 1.2 }),
+            withTiming(1.2, { duration: MATCH_STEP_DURATION * 1.4 }),
+            withTiming(0.9, { duration: MATCH_STEP_DURATION * 1.6 }),
+            withTiming(1, { duration: MATCH_STEP_DURATION * 1.8 }),
+          ),
+        },
+        {
+          scaleY: withSequence(
+            withTiming(0.6, { duration: MATCH_STEP_DURATION }),
+            withTiming(1.3, { duration: MATCH_STEP_DURATION * 1.2 }),
+            withTiming(0.8, { duration: MATCH_STEP_DURATION * 1.4 }),
+            withTiming(1.1, { duration: MATCH_STEP_DURATION * 1.6 }),
+            withTiming(1, { duration: MATCH_STEP_DURATION * 1.8 }),
+          ),
+        },
+      ],
+    }
+  })
+  return { runMatchAnimation, matchAnimationStyle }
+}
+
+function useNoMatchAnimation() {
+  const runNoMatchAnimation = useSharedValue(false)
+  const noMatchAnimationStyle = useAnimatedStyle(() => {
+    if (!runNoMatchAnimation.value) {
+      return {
+        transform: [{ translateX: 0 }, { rotateZ: '0deg' }],
+      }
+    }
+    return {
+      transform: [
+        {
+          translateX: withSequence(
+            withTiming(15, { duration: NO_MATCH_STEP_DURATION }),
+            withTiming(-11.25, { duration: NO_MATCH_STEP_DURATION }),
+            withTiming(7.5, { duration: NO_MATCH_STEP_DURATION }),
+            withTiming(-3.75, { duration: NO_MATCH_STEP_DURATION }),
+            withTiming(0, { duration: NO_MATCH_STEP_DURATION }),
+          ),
+        },
+        {
+          rotateZ: withSequence(
+            withTiming('6deg', { duration: NO_MATCH_STEP_DURATION }),
+            withTiming('-4.5deg', { duration: NO_MATCH_STEP_DURATION }),
+            withTiming('3deg', { duration: NO_MATCH_STEP_DURATION }),
+            withTiming('-1.5deg', { duration: NO_MATCH_STEP_DURATION }),
+            withTiming('0deg', { duration: NO_MATCH_STEP_DURATION }),
+          ),
+        },
+      ],
+    }
+  })
+  return { runNoMatchAnimation, noMatchAnimationStyle }
+}
+
 interface CardViewProps {
   card: Card
   cardSize: number
@@ -22,71 +91,10 @@ interface CardViewProps {
 
 export const CardView = observer(
   ({ card, cardSize, gapSize }: CardViewProps) => {
-    const runMatchAnimation = useSharedValue(false)
-    const matchAnimationStyle = useAnimatedStyle(() => {
-      // Code here runs on the UI thread, so it will be synchronous
-      if (!runMatchAnimation.value) {
-        return {
-          transform: [{ scaleX: 1 }, { scaleY: 1 }],
-        }
-      }
-      return {
-        transform: [
-          {
-            scaleX: withSequence(
-              withTiming(1.4, { duration: MATCH_STEP_DURATION }),
-              withTiming(0.7, { duration: MATCH_STEP_DURATION * 1.2 }),
-              withTiming(1.2, { duration: MATCH_STEP_DURATION * 1.4 }),
-              withTiming(0.9, { duration: MATCH_STEP_DURATION * 1.6 }),
-              withTiming(1, { duration: MATCH_STEP_DURATION * 1.8 }),
-            ),
-          },
-          {
-            scaleY: withSequence(
-              withTiming(0.6, { duration: MATCH_STEP_DURATION }),
-              withTiming(1.3, { duration: MATCH_STEP_DURATION * 1.2 }),
-              withTiming(0.8, { duration: MATCH_STEP_DURATION * 1.4 }),
-              withTiming(1.1, { duration: MATCH_STEP_DURATION * 1.6 }),
-              withTiming(1, { duration: MATCH_STEP_DURATION * 1.8 }),
-            ),
-          },
-        ],
-      }
-    })
-    if (card.isMatched) {
-      runMatchAnimation.value = true
-    }
+    const { runMatchAnimation, matchAnimationStyle } = useMatchAnimation()
+    runMatchAnimation.value = card.isMatched
 
-    const runNoMatchAnimation = useSharedValue(false)
-    const noMatchAnimationStyle = useAnimatedStyle(() => {
-      if (!runNoMatchAnimation.value) {
-        return {
-          transform: [{ translateX: 0 }, { rotateZ: '0deg' }],
-        }
-      }
-      return {
-        transform: [
-          {
-            translateX: withSequence(
-              withTiming(15, { duration: NO_MATCH_STEP_DURATION }),
-              withTiming(-11.25, { duration: NO_MATCH_STEP_DURATION }),
-              withTiming(7.5, { duration: NO_MATCH_STEP_DURATION }),
-              withTiming(-3.75, { duration: NO_MATCH_STEP_DURATION }),
-              withTiming(0, { duration: NO_MATCH_STEP_DURATION }),
-            ),
-          },
-          {
-            rotateZ: withSequence(
-              withTiming('6deg', { duration: NO_MATCH_STEP_DURATION }),
-              withTiming('-4.5deg', { duration: NO_MATCH_STEP_DURATION }),
-              withTiming('3deg', { duration: NO_MATCH_STEP_DURATION }),
-              withTiming('-1.5deg', { duration: NO_MATCH_STEP_DURATION }),
-              withTiming('0deg', { duration: NO_MATCH_STEP_DURATION }),
-            ),
-          },
-        ],
-      }
-    })
+    const { runNoMatchAnimation, noMatchAnimationStyle } = useNoMatchAnimation()
     runNoMatchAnimation.value = card.isNotMatched
 
     return (
