ff76b7644c05aa6735eb2546b11b6c55372eae1c
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2024-02-29 23:21:14 +0100

    InfoModal - add link to GitHub and app description

--- src/component/InfoModal.tsx
+++ src/component/InfoModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Pressable, StyleSheet, Text, View } from 'react-native'
+import { Linking, Modal, Pressable, StyleSheet, Text, View } from 'react-native'
 import { Color } from '../style/Color'
 import React from 'react'
 
@@ -23,8 +23,29 @@ export function InfoModal({ onClose }: Props) {
             X
           </Text>
         </Pressable>
-        <Text>Hi!</Text>
-        <Text>You can find the source code on GitHub:</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>Hi!</Text>
+          <Text style={styles.text}>
+            This app is built with React Native, TypeScript and MobX for state
+            management.
+          </Text>
+          <Text style={styles.text}>
+            Animations and gestures are implemented with Animated, Reanimated
+            and PanResponder.
+          </Text>
+          <Text style={styles.text}>
+            You can find the source code on GitHub at{' '}
+            <Text
+              style={styles.link}
+              onPress={() =>
+                Linking.openURL(
+                  'https://github.com/AlbertVilaCalvo/React-Native-Memory-Game',
+                )
+              }>
+              https://github.com/AlbertVilaCalvo/React-Native-Memory-Game
+            </Text>
+          </Text>
+        </View>
       </View>
     </Modal>
   )
@@ -47,5 +68,17 @@ const styles = StyleSheet.create({
   },
   closeText: {
     fontSize: 20,
+  },
+  textContainer: {
+    flex: 0.75,
+    justifyContent: 'center',
+  },
+  text: {
+    color: Color.dark,
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  link: {
+    textDecorationLine: 'underline',
   },
 })
